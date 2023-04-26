dbbd6a3da170243d819d401a455d2639106375f4
OmarHas07 <omh0012@auburn.edu>
2023-04-26 13:46:27 -0500

    editing the script

--- script.py
+++ script.py
@@ -1,18 +1,36 @@
-def count_inherit_words(filename):
-    with open(filename, 'r') as file:
-        text = file.read()
-    words = text.lower().split()
-    inherit_words = [word for word in words if word.startswith('inherit')]
-    word_counts = {}
-    for word in inherit_words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-    for word, count in word_counts.items():
-        print("{}\t{}".format(count, word.capitalize()))
+import re
+import sys
 
-count_inherit_words('origin.txt')
+def read_file(file_path):
 
-	
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return lines
 
+def find_heritability_words(lines):
+
+    pattern = re.compile(r'\b(heritable|inherit(?:ed|s|ance|ors?|ing|able)?|heritability)\b', re.IGNORECASE)
+    results = []
+
+    for index, line in enumerate(lines, start=1):
+        matches = pattern.findall(line)
+        for match in matches:
+            results.append((index, match))
+
+    return results
+
+def write_results(results, output_file):
+
+    with open(output_file, 'w') as file:
+        for line_number, word in results:
+            file.write(f'{line_number}\t{word}\n')
+
+def main():
+    input_file = 'origin.txt'
+    output_file = 'heritability_results.txt'
+    lines = read_file(input_file)
+    results = find_heritability_words(lines)
+    write_results(results, output_file)
+
+if __name__ == '__main__':
+    main()
